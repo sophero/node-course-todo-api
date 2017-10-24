7b91e58458f09d672847bb0aea52bcc05e2ce147
Sophia Di <backwardsxmarathon@gmail.com>
2017-10-24 15:41:43 -0400

    Add post todos route and refactor mongoose.

---
+++ server/models/todo.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true // removes leading and trailing white space
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+
+module.exports = {Todo};
